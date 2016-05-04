ba648dab25ea3e2290ad9e83e9ad7db0bf3d7c3b
lucasam <lucasam@gmail.com>
2016-05-04 10:20:25 -0300

    Adding basic tests to gwt jackson encoders

--- restygwt/src/test/java/org/fusesource/restygwt/GwtCompleteTestSuite.java
+++ restygwt/src/test/java/org/fusesource/restygwt/GwtCompleteTestSuite.java
@@ -18,9 +18,6 @@
 
 package org.fusesource.restygwt;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-
 import org.fusesource.restygwt.client.basic.BasicGwtJacksonTestGwt;
 import org.fusesource.restygwt.client.basic.BasicTestGwt;
 import org.fusesource.restygwt.client.basic.CacheCallbackTestGwt;
@@ -53,6 +50,9 @@ import org.fusesource.restygwt.client.complex.JsonTypeIdResolver;
 import org.fusesource.restygwt.client.complex.StringEncoderDecoderTestGwt;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
 
 /**
  *
